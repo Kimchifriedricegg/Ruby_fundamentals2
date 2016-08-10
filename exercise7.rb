e41b9c78fc715b6f1e6fd8849ba19d121c6fedf6
Kimchifriedricegg <alexovidenie@gmail.com>
2016-08-10 16:47:22 -0400

    deleted cohort2

--- exercise7.rb
+++ exercise7.rb
@@ -3,6 +3,9 @@ students = { :cohort1 => 34, :cohort2 => 42, :cohort3 => 22 }
 # added cohort4 to hash
 students[:cohort4]= 43
 
+# delete 2nd cohort and redisplay
+students.delete(:cohort2)
+
 students.each do |key, value|
   students[key] = (value * 1.05)
   puts "#{key} is #{students[key]}"
